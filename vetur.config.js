0573b274c5096a8a7b75623ffa5deaf714885865
miyabin66 <freezemark5166@gmail.com>
2022-04-29 15:49:25 +0900

    lint関連整備

--- vetur.config.js
+++ vetur.config.js
@@ -4,8 +4,8 @@ module.exports = {
   // override vscode settings
   // Notice: It only affects the settings used by Vetur.
   settings: {
-    "vetur.useWorkspaceDependencies": true,
-    "vetur.experimental.templateInterpolationService": true
+    'vetur.useWorkspaceDependencies': true,
+    'vetur.experimental.templateInterpolationService': true,
   },
   // **optional** default: `[{ root: './' }]`
   // support monorepos
@@ -32,8 +32,7 @@ module.exports = {
       // If you set it, you can get completion by that components.
       // It is relative to root property.
       // Notice: It won't actually do it. You need to use `require.context` or `Vue.component`
-      globalComponents: [
-      ]
-    }
-  ]
+      globalComponents: [],
+    },
+  ],
 }
