deff3bc6e8ec4fa9115793248cc3191ef4464033
miyabin66 <freezemark5166@gmail.com>
2022-04-29 15:30:25 +0900

    modern-css-resetからreset-cssに変更

--- src/renderer/nuxt.config.ts
+++ src/renderer/nuxt.config.ts
@@ -87,7 +87,7 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: ['reset-css'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
